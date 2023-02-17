86a79c59680bf5f28bbebf95e5f16a28c0475d9f
froehlicha <froehlich.alexander@protonmail.com>
2023-02-17 22:22:00 +0100

    add colors to alacritty

--- home/features/alacritty.nix
+++ home/features/alacritty.nix
@@ -1,3 +1,16 @@
+{ colors, inputs, ... }:
 {
   programs.alacritty.enable = true;
+
+  programs.alacritty.settings.colors = with colors.withHashtag;
+    let
+      default = {
+        black = base00; white = base07;
+        inherit red green yellow blue cyan magenta;
+      };
+    in {
+      primary = { background = base00; foreground = base07; };
+      cursor = { text = base02; cursor = base07; };
+      normal = default; bright = default; dim = default;
+    };
 }
