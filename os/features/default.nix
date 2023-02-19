b37b46bcc35f21d39d25036f071a836bd23dde7e
froehlicha <froehlich.alexander@protonmail.com>
2023-02-20 00:59:41 +0100

    enable pipewire

--- os/features/default.nix
+++ os/features/default.nix
@@ -3,6 +3,7 @@
   imports = [
     inputs.home-manager.nixosModules.home-manager
     ../../modules/os/colors.nix
+    ./audio.nix
     ./colors.nix
     ./greetd.nix
     ./intl.nix
--- os/features/wayland.nix
+++ os/features/wayland.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-  services.pipewire.enable = true;
-
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
