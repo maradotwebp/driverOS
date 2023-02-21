75e8c9b6a697af77c7ce7bb2e3a1f637974b48fb
froehlicha <froehlich.alexander@protonmail.com>
2023-02-21 19:16:38 +0100

    add libnotify support to screenshot.nix

--- home/features/screenshot.nix
+++ home/features/screenshot.nix
@@ -7,6 +7,7 @@
         mkdir shots
         FILEOUT="shots/$(date +'%Y-%m-%d-%H%M%S_screenshot.png')"
         ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp -d)" - | ${pkgs.swappy}/bin/swappy -f - -o "$FILEOUT"
+        ${pkgs.libnotify}/bin/notify-send "Screenshot saved at ~/shots"
       ''}";
     };
   };
