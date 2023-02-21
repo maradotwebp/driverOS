36836d72e8ac2a71dfedd4199c5605feb0060ccb
froehlicha <froehlich.alexander@protonmail.com>
2023-02-21 18:32:22 +0100

    fix incorrect nixsearch icon

--- home/features/nixsearch.nix
+++ home/features/nixsearch.nix
@@ -3,17 +3,14 @@
   xdg.desktopEntries = {
     "nixos-options-search" = {
       name = "Search NixOS Options";
-      icon = "bluetooth";
       exec = "${pkgs.firefox}/bin/firefox --new-window \"https://search.nixos.org/options\"";
     };
     "nixos-package-search" = {
       name = "Search NixOS Packages";
-      icon = "bluetooth";
       exec = "${pkgs.firefox}/bin/firefox --new-window \"https://search.nixos.org/packages\"";
     };
     "home-manager-options-search" = {
       name = "Search HomeManager Options";
-      icon = "bluetooth";
       exec = "${pkgs.firefox}/bin/firefox --new-window \"https://mipmip.github.io/home-manager-option-search/\"";
     };
   };
