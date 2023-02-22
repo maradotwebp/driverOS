7f29218c4529af36e2afc3997b702a7744debaa5
froehlichA <froehlich.alexander@protonmail.com>
2023-02-22 14:46:34 +0100

    add cpu & gpu support to prince

--- os/prince/default.nix
+++ os/prince/default.nix
@@ -2,10 +2,17 @@
 {
   imports = [
     ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
 
     ../features
     ../features/optional/uefi.nix
   ];
+
+  hardware.nvidia.prime = {
+    nvidiaBusId = "PCI:01:00:0";
+    amdgpuBusId = "PCI:13:00:0";
+  };
 
   networking.hostName = "prince";
   system.stateVersion = "23.05"; # LEAVE THIS ALONE (see https://nixos.org/nixos/options.html)
