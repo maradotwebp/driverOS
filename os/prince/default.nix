92616eb239e9a11925d463898ac503ee8fb41944
froehlicha <froehlich.alexander@protonmail.com>
2023-02-25 17:28:15 +0100

    add databases to prince

--- os/prince/default.nix
+++ os/prince/default.nix
@@ -11,13 +11,18 @@
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = pkgs.linuxKernel.packages.linux_5_15.nvidia_x11;
     modesetting.enable = true;
   };
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
   };
   boot.blacklistedKernelModules = [ "amdgpu" ];
+
+  # Services
+  services.mongodb.enable = true;
+  services.redis.servers."dev".enable = true;
+  services.postgresql.enable = true;
 
   networking.hostName = "prince";
   system.stateVersion = "23.05"; # LEAVE THIS ALONE (see https://nixos.org/nixos/options.html)
